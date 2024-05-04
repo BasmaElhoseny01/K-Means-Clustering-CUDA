79f5c76a06ed444fbd2bb58f0a50ec7c17fecef2
BasmaElhoseny <basmaelhoseny6@gmail.com>
2024-05-04 17:31:14 +0300

    Create gpu.cu

---
+++ gpu.cu
@@ -0,0 +1,141 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <fstream>
+#include <assert.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <float.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
+int K = 2;
+
+__host__ float *read_image(char *path, int *width, int *height, int *channels)
+{
+
+    // Read Image
+    unsigned char *image_data = stbi_load(path, width, height, channels, 0);
+
+    if (image_data == NULL)
+    {
+        printf("Error loading image\n");
+        exit(1);
+    }
+    if (*channels != 1)
+    {
+        printf("Error: Image should be grayscale: %d\n", *channels);
+        exit(1);
+    }
+
+    // Host Memory Allocation & convert data from unsigned char to float
+    float *image = (float *)malloc(sizeof(float) * (*width) * (*height) * (*channels));
+
+    // Normlaization
+    for (int i = 0; i < (*height) * (*width) * (*channels); i++)
+    {
+        image[i] = (float)image_data[i] / 255.0f;
+    }
+
+    if (*image == NULL)
+    {
+        printf("Error loading image\n");
+        exit(1);
+    }
+
+    // Free the loaded image
+    stbi_image_free(image_data);
+
+    printf("Image loaded successfully\n");
+
+    // for (int i = 0; i < (*height) * (*width) * (*channels); i++)
+    // {
+    //     printf("%f ", image[i]);
+    // }
+
+    return image;
+}
+
+__host__ float *intilize_centroids(int N, int D, int K, float *data_points)
+{
+    /*
+    Function to initialize centroids randomly as one of the data points
+
+    args:
+    N: number of data points
+    D: number of dimensions
+    K: number of clusters
+    data_points: data points as a 1D array
+
+    returns: centroids as a 1D array
+    */
+    srand(time(NULL)); // Seed for randomization
+
+    float *centroids = (float *)malloc(K * D * sizeof(float));
+    for (int i = 0; i < K; i++)
+    {
+        // Each centroid is initialized to a Random data point
+        int i_random = rand() % N;
+        for (int j = 0; j < D; j++)
+        {
+            centroids[i * D + j] = data_points[i_random * D + j];
+        }
+    }
+
+    printf("Centroids initialized successfully :D\n");
+
+    // for (int i = 0; i < K; i++)
+    // {
+    //     for (int j = 0; j < D; j++)
+    //     {
+    //         printf("%f ", centroids[i * D + j]);
+    //     }
+    //     printf("\n");
+    // }
+
+    return centroids;
+}
+/*
+Kmeans:
+1. Initialize centroids (Random or Kmeans++)
+2. Assign each data point to the nearest centroid
+3. Update the centroids
+4. Repeat 2 and 3 until convergence
+*/
+int main(int argc, char *argv[])
+{
+
+    printf("Hello World\n");
+
+    // Input Arguments
+    if (argc != 3)
+    {
+        printf("Usage: %s <input_file>", argv[0]);
+        exit(1);
+    }
+
+    char *input_file_path = argv[1];
+    // K = atoi(argv[2]);
+
+    printf("Input file path: %s\n", input_file_path);
+
+    // Read image
+    int width, height, channels;
+    float *image = read_image(input_file_path, &width, &height, &channels);
+
+    int N = width * height; // no of data points
+    int D = channels;       // no of dimensions [1 as start]
+
+    // Initialize centroids
+    float *centroids = intilize_centroids(N, D, K, image);
+}
+
+
+// nvcc -o out_gpu_1  ./gpu.cu
+// ./out_gpu_1 ./input.png 2
