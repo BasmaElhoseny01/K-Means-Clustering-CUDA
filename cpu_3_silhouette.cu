86fcad9148f56e4b13bacc019ebe719d594c0776
BasmaElhoseny <basmaelhoseny6@gmail.com>
2024-05-19 08:15:48 +0300

    Update cpu_3_silhouette.cu

--- cpu_3_silhouette.cu
+++ cpu_3_silhouette.cu
@@ -554,7 +554,7 @@ int main(int argc, char *argv[])
 
     // Save the clustered image
     std::string input_path(input_file_path);
-    std::string output_path = input_path.substr(0, input_path.find_last_of('.')) + "_output_cpu.png";
+    std::string output_path = input_path.substr(0, input_path.find_last_of('.')) + "_output_cpu_3_silhouette.png";
     stbi_write_png(output_path.c_str(), width, height, 3, clutsered_image, width * 3);
     printf("Image saved successfully at: %s\n", output_path.c_str());
 
